8b915f356c342dbca28861ff09b5cb8e22b977c9
tutugodfrey <godfrey_tutu@yahoo.com>
2026-08-18 10:14:22 +0100

    Update signup validation to return array of objects for failed validation

--- src/middlewares/validation/auth.js
+++ src/middlewares/validation/auth.js
@@ -1,14 +1,21 @@
 export default {
   signup: (req, res, next) => {
     let count = 0
+    let validations = []
     const fields = ['password', 'confirmPassword', 'name', 'email', 'username'];
     fields.forEach(field => {
-      if (!req.body[field]) 
-      return res.status(400).json({
-        message:  `${field} is required to sign up`, 
-      });
+      if (!req.body[field]) {
+        validations.push({
+          field,
+          message: `${field} is required to sign up`
+        })
+      }
       count++
     });
+
+    if (validations.length > 0){
+      return res.status(400).json(validations)
+    }
 
     // check that all fields are validated 
     if (fields.length === count) {
